2e58f8ccccdf2bd4f003567214421a7f14d841fb
giriraj171 <giriraj171@gmail.com>
2022-10-20 17:24:26 +0530

    added 30 to main

--- loginuser.java
+++ loginuser.java
@@ -2,7 +2,7 @@ public class login
 {
         public static void main(string args[])
         {
-                int a=10;
+                int a=30;
                 for (i-0; 1<=10; i++)
                 {
                 system out. printin("number: "+a);
